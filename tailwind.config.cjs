95e9c6b7b0f614e44b18ca38ceb4a4ed619c68c8
wecessary <92378671+wecessary@users.noreply.github.com>
2023-08-02 18:05:58 +0100

    Add pricing (#7)
    * add pricing plans
    
    * fix: custom is not a one-off fee

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,6 +6,16 @@ module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
+      listStyleImage: {
+        checkmark: 'url("/images/check-mark.svg")',
+      },
+      colors: {
+        "theme-red": "#FA8787",
+        "theme-orange": "#FF9245",
+        "theme-blue": "#5083FF",
+        "theme-purple": "#6E6CCC",
+        "theme-amber": "FF660B",
+      },
       fontFamily: {
         sans: ["Futura", ...defaultTheme.fontFamily.sans],
       },
