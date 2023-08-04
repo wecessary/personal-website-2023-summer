acec2ddb90f9082b3fa662d0acc6fe84f9d659cc
wecesscary <wesleymhj@gmail.com>
2023-08-04 10:02:52 +0100

    restyle: strikethrough animation

--- src/scripts/observer.ts
+++ src/scripts/observer.ts
@@ -1,6 +1,6 @@
 import typewriter from "./typewritterEffect";
 
-let strikeThroughTarget = document.querySelector(".should-strike-through");
+let strikeThroughTarget = document.querySelector(".animate-strike");
 let typewriterTarget = document.querySelector(".typewriter-target");
 let diamondTarget = document.querySelector(".diamond-target");
 let testimonialTarget = document.querySelector(".testimonial-target");
@@ -9,25 +9,28 @@ let hasCalledTypewriter = false;
 let io = new IntersectionObserver(
   (entries) => {
     entries.forEach((entry) => {
-      const classList = entry.target.classList;
+      const entryClassList = entry.target.classList;
       if (!entry.isIntersecting) return;
 
-      if (classList.contains("should-strike-through")) {
-        classList.add("animate-strike");
+      if (entryClassList.contains("animate-strike")) {
+        entryClassList.remove("animate-paused");
       }
 
-      if (!hasCalledTypewriter && classList.contains("typewriter-target")) {
+      if (
+        !hasCalledTypewriter &&
+        entryClassList.contains("typewriter-target")
+      ) {
         hasCalledTypewriter = true;
         typewriter("codes");
       }
 
-      if (classList.contains("testimonial-target")) {
+      if (entryClassList.contains("testimonial-target")) {
         document
           .querySelector(".animate-testimonial")
           ?.classList.remove("animate-paused");
       }
 
-      if (classList.contains("diamond-target")) {
+      if (entryClassList.contains("diamond-target")) {
         document
           .querySelector(".animate-diamond")
           ?.classList.remove("animate-paused");
