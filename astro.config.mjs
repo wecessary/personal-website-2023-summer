b024503decc90f3dff7d46a95fef9d480758edff
wecesscary <wesleymhj@gmail.com>
2023-08-07 11:49:39 +0100

    use prettier --write

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,12 @@ import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind({
-    // Example: Disable injecting a basic `base.css` import on every page.
-    // Useful if you need to define and/or import your own custom `base.css`.
-    applyBaseStyles: false
-  }), preact()]
+  integrations: [
+    tailwind({
+      // Example: Disable injecting a basic `base.css` import on every page.
+      // Useful if you need to define and/or import your own custom `base.css`.
+      applyBaseStyles: false,
+    }),
+    preact(),
+  ],
 });
--- src/const/planFeatures.ts
+++ src/const/planFeatures.ts
@@ -2,12 +2,12 @@ export const starterFeatures = [
   {
     text: "Simple and modern designs",
     tooltip: "Clean and modern designs that focus on simplicity and usability.",
-    ariaDescribedBy:"simple-and-modern-designs-tooltip"
+    ariaDescribedBy: "simple-and-modern-designs-tooltip",
   },
   {
     text: "Mobile friendly",
     tooltip: "Your website will look great on all devices.",
-    ariaDescribedBy:"mobile-friendly-tooltip"
+    ariaDescribedBy: "mobile-friendly-tooltip",
   },
   { text: "Lifetime Gurantee" },
   { text: "Hosting" },
@@ -15,8 +15,7 @@ export const starterFeatures = [
     text: "CMS",
     tooltip:
       "You will be able to change the content of your website without touching the code.",
-    ariaDescribedBy:"cms-tooltip"
-
+    ariaDescribedBy: "cms-tooltip",
   },
   { text: "Basic SEO" },
   { text: "Enquiry form" },
@@ -29,14 +28,14 @@ export const plusFeatures = [
   {
     text: "Visually engaging designs",
     tooltip: "Polished designs that reflect your brand.",
-    ariaDescribedBy:"visually-engaging-designs-tooltip"
+    ariaDescribedBy: "visually-engaging-designs-tooltip",
   },
   { text: "Design mockups" },
   { text: "Full SEO" },
   {
     text: "1-2 custom functionalities",
     tooltip: "E.g. login portal, search engine, and AI chatbot.",
-    ariaDescribedBy:"plus-custom-functionalities-tooltip"
+    ariaDescribedBy: "plus-custom-functionalities-tooltip",
   },
 ];
 export const proFeatures = [
@@ -45,12 +44,12 @@ export const proFeatures = [
     text: "For websites with more pages",
     tooltip:
       "Pages that share the same design are always free. For more see below.",
-    ariaDescribedBy:"pro-for-websites-with-more-pages-tooltip"
+    ariaDescribedBy: "pro-for-websites-with-more-pages-tooltip",
   },
   {
     text: "1-4 custom functionalities",
     tooltip: "E.g. login portal, search engine, and AI chatbot.",
-    ariaDescribedBy:"pro-custom-functionalities-tooltip"
+    ariaDescribedBy: "pro-custom-functionalities-tooltip",
   },
 ];
 export const enterpriseFeatures = [
